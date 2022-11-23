ac8f546e89bcb8115f1be24e96813f911ed677dc
Rishab Singh <rishab.w.singh1234@gmail.com>
2022-11-23 15:05:01 +0530

    Pizza Bill

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,11 +3,13 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
+    dp.addExtraToppings();
     dp.addTakeaway();
     System.out.println(dp.getBill());
+
+    System.out.println();
 
     Pizza p = new Pizza(true);
     p.addExtraCheese();
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,8 +6,14 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int cheesePrice = 0;
+    private int toppingPrice = 0;
+    private int addTakeaway = 0;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg) this.price = 300;
+        else this.price = 400;
         // your code goes here
     }
 
@@ -17,18 +23,28 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        this.cheesePrice = 80;
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg) this.toppingPrice = 70;
+        else this.toppingPrice = 120;
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.addTakeaway = 20;
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        String ans = "Base Price Of The Pizza: "+price+"\n";
+        if(cheesePrice > 0) ans += "Extra Cheese Added: "+cheesePrice+"\n";
+        if(toppingPrice > 0) ans += "Extra Toppings Added: "+toppingPrice+"\n";
+        if(addTakeaway > 0) ans += "Paperbag Added: "+addTakeaway+"\n";
+        ans += "Total Price: "+(price+cheesePrice+toppingPrice+addTakeaway);
+        return ans;
     }
 }
